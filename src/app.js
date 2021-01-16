7815700a02929bb55f9b00289f3cbed450595c38
teja <tejareddy.teju@gmail.com>
2021-01-16 17:16:41 +0530

    Organizing Redux

--- src/app.js
+++ src/app.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import AppRouter from './routers/AppRouter';
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water bill', amount: 2000}));
+store.dispatch(addExpense({ description: 'Gas bill', amount: 3000 }));
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
