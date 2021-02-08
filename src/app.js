730abdb24bd383d75318c263f481f914139375fd
teja <tejareddy.teju@gmail.com>
2021-02-08 14:54:05 +0530

    dropdown for picking sortBy

--- src/app.js
+++ src/app.js
@@ -12,13 +12,9 @@ import AppRouter from './routers/AppRouter';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 2000}));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 3000 }));
-store.dispatch(setTextFilter('water'));
-
-setTimeout(()=> {
-  store.dispatch(setTextFilter('bill'));
-}, 3000);
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500}));
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500}));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
