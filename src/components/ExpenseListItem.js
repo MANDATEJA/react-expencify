fa2a8f9c0ef71abb2edfd05292434d40ffcf13cd
teja <tejareddy.teju@gmail.com>
2021-02-11 14:46:29 +0530

    Wiring up editExpense page

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-import { removeExpense } from '../actions/expenses';
-
-const ExpenseListItem =  ({ dispatch, id, description, amount, createdAt }) => (
+const ExpenseListItem =  ({ id, description, amount, createdAt }) => (
   <div>
-    <h3>Description: {description}</h3>
+    <Link to={`/edit/${id}`}>
+      <h3>Description: {description}</h3>
+    </Link>
     <p>{amount} - {createdAt}</p>
-    <button onClick={(e) => {
-      dispatch(removeExpense({ id }));
-    }} >Reomove</button>
   </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
